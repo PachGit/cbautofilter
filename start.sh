68133ad60841aa0c7a8084f9a7259e9b538df2ca
PachGit <108862443+PachGit@users.noreply.github.com>
2022-08-06 13:20:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/cinemavillaz/Alexa-V2.git /Alexa-V2
+  git clone https://github.com/PachGit/cbautofilter.git /cbautofilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Alexa-V2
+  git clone $UPSTREAM_REPO /cbautofilter
 fi
-cd /Alexa-V2
+cd /cbautofilter
 pip3 install -U -r requirements.txt
 echo "Starting....🔥"
 python3 bot.py
